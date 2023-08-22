626aea200f9f5de10316695e8406b4535c21a030
kimoVoid <bordakmc@gmail.com>
2023-08-22 15:48:26 -0300

    added saveUnloadChunks rule

---
+++ src/main/java/net/nullspace_mc/tapestry/helpers/ChunkMapHelper.java
@@ -0,0 +1,8 @@
+package net.nullspace_mc.tapestry.helpers;
+
+import net.minecraft.util.Long2ObjectHashMap;
+
+public interface ChunkMapHelper {
+
+	Long2ObjectHashMap getChunks();
+}
---
+++ src/main/java/net/nullspace_mc/tapestry/helpers/ServerChunkCacheHelper.java
@@ -0,0 +1,8 @@
+package net.nullspace_mc.tapestry.helpers;
+
+import java.util.List;
+
+public interface ServerChunkCacheHelper {
+
+	List getChunks();
+}
